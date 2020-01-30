4b7af8087eb3bbcadef04a2f6303c15ee29a78e4
spacecowboi <cunningham2192@gmail.com>
2020-01-30 12:04:51 -0800

    fixed pathing issues

--- app.js
+++ app.js
@@ -121,26 +121,26 @@ var handleClickonProduct = function (event){
 //building new instances for constructor function
 
 //instantiation of new objects
-new Product ('bag', '/img/bag.jpg');
-new Product ('banana', '/img/banana.jpg');
-new Product ('bathroom', '/img/bathroom.jpg');
-new Product ('boots', '/img/boots.jpg');
-new Product ('breakfast', '/img/breakfast.jpg');
-new Product ('bubblegum', '/img/bubblegum.jpg');
-new Product ('chair', '/img/chair.jpg');
-new Product ('cthulhu', '/img/cthulhu.jpg');
-new Product ('dog-duck', '/img/dog-duck.jpg');
-new Product ('dragon', '/img/dragon.jpg');
-new Product ('pen', '/img/pen.jpg');
-new Product ('pet-sweep.jpg', '/img/pet-sweep.jpg');
-new Product ('scissors', '/img/scissors.jpg');
-new Product ('shark', '/img/shark.jpg');
-new Product ('sweep', '/img/sweep.png');
-new Product ('tauntaun', '/img/tauntaun.jpg');
-new Product ('unicorn', '/img/unicorn.jpg');
-new Product ('usb', '/img/usb.gif');
-new Product ('water-can', '/img/water-can.jpg');
-new Product ('wine-glass', '/img/wine-glass.jpg');
+new Product ('bag', 'img/bag.jpg');
+new Product ('banana', 'img/banana.jpg');
+new Product ('bathroom', 'img/bathroom.jpg');
+new Product ('boots', 'img/boots.jpg');
+new Product ('breakfast', 'img/breakfast.jpg');
+new Product ('bubblegum', 'img/bubblegum.jpg');
+new Product ('chair', 'img/chair.jpg');
+new Product ('cthulhu', 'img/cthulhu.jpg');
+new Product ('dog-duck', 'img/dog-duck.jpg');
+new Product ('dragon', 'img/dragon.jpg');
+new Product ('pen', 'img/pen.jpg');
+new Product ('pet-sweep.jpg', 'img/pet-sweep.jpg');
+new Product ('scissors', 'img/scissors.jpg');
+new Product ('shark', 'img/shark.jpg');
+new Product ('sweep', 'img/sweep.png');
+new Product ('tauntaun', 'img/tauntaun.jpg');
+new Product ('unicorn', 'img/unicorn.jpg');
+new Product ('usb', 'img/usb.gif');
+new Product ('water-can', 'img/water-can.jpg');
+new Product ('wine-glass', 'img/wine-glass.jpg');
 
 // building the event listener
 
